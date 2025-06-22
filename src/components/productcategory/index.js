c52dcc63b391fef204e53b0f78161f0b0542def8
Raphael Torsu <torsu.pilgrim@gmail.com>
2025-06-22 16:33:38 +0000

    product category page

--- src/components/productcategory/index.js
+++ src/components/productcategory/index.js
@@ -3,15 +3,18 @@ import AltSeeProductButton from "../ui/AltSeeProductButton";
 import HomepageProductListing from "../homepage/HP-ProductListing";
 import ProductDescriptionGroup from "../homepage/HP-ProductDescriptionGroup";
 import { useNavigate } from "react-router-dom";
+import { useWindowWidth } from "../../custom-hooks"
 
 export default function ProductCategoryPage({ type }) {
   const filteredDataList = dataList.filter(item => item.category === type)
   const navigate = useNavigate();
-  
+  const width = useWindowWidth();
+  const isMobile = width < 768;
+
   return (
     <div>
       <div style={{
-        background: "#181818",
+        background: "#000",
         display: "flex",
         flexDirection: "row",
         justifyContent: "center",
@@ -21,16 +24,7 @@ export default function ProductCategoryPage({ type }) {
       }}>
         <h2 style={{ textTransform: "uppercase", color: "#fff"}}>{type}</h2>
       </div>
-      <ul style={{ 
-        listStyle: "none", 
-        padding: "0px 200px", 
-        margin: "120px 0px",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        gap: "120px",
-      }}>
+      <ul className="pcp-sect-one">
         {
           filteredDataList.map((filteredData, index) => {
             const isEven = filteredData.id % 2 === 0;
@@ -39,29 +33,31 @@ export default function ProductCategoryPage({ type }) {
                 src={filteredData.image.desktop} 
                 alt="Product Category Item" 
                 style={{
-                  width: "30%"
+                  width: isMobile ? "100%" : "30%",
+                  borderRadius: "10px"
                 }}
               />
             )
             const descElement = (
               <div style={{
-                width: "48%", 
+                width: isMobile ? "100%" : "48%", 
                 display: "flex", 
                 flexDirection: "column", 
                 alignItems: "start", 
                 justifyContent: "center", 
                 gap: "2px"
               }}>
-                { filteredData.new ? <p style={{ color: "#d87d4a"}}>NEW PRODUCT</p> : <></> }
+                { filteredData.new ? <p style={{ color: "#d87d4a", width: "100%"}} className="overline">NEW PRODUCT</p> : <></> }
                 <h1 style={{ 
-                  fontSize: "3rem", 
+                  fontSize: isMobile ? "1.6rem" : "3rem", 
                   fontWeight: "bold", 
                   margin: 0, 
                   lineHeight: 1.1, 
-                  textAlign: "start" 
+                  textAlign: isMobile ? "center" : "start",
+                  width: "100%"
                 }}>{filteredData.name}</h1>
-                <p style={{ textAlign: "start", opacity: "0.5"}}>{filteredData.description}</p>
-                <div style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
+                <p style={{ textAlign: isMobile ? "center" : "start", opacity: "0.5"}}>{filteredData.description}</p>
+                <div style={{display: "flex", alignItems: "center", justifyContent: "center", width: "100%"}}>
                   <AltSeeProductButton onClickFn={() => navigate(`/product/${filteredData.id}`)}/>
                 </div>
               </div>
@@ -69,25 +65,34 @@ export default function ProductCategoryPage({ type }) {
             return (
               <li 
                 key={filteredData.id}
-                style={{ display: "flex", flexDirection: "row", gap: "10%"}}
+                style={{ display: "flex", flexDirection: isMobile ? "column" : "row", gap: isMobile ? "20px" : "10%"}}
               >
-                {isEven ? (
+                {!isMobile && isEven && (
                   <>
                     {imageElement}
                     {descElement}
                   </>
-                ) : (
+                )}
+                {!isMobile && !isEven && (
                   <>
                     {descElement}
                     {imageElement}
                   </>
                 )}
+                {
+                  isMobile && (
+                    <>
+                      {imageElement}
+                      {descElement}
+                    </>
+                  )
+                }
               </li>
             )
           })
         }
       </ul>
-      <HomepageProductListing />
+      <HomepageProductListing stylesVal={{marginTop: "170px"}}/>
       <ProductDescriptionGroup />
     </div>
   );
